d64b9c3af0d5f91bbe50b88d257839ce81db48b5
hamstar0 <webutek@gmail.com>
2020-04-15 03:34:13 -0700

    v1.0.0 - Changed recipe to a pre-hardmode variation

--- Emitters/Items/EmitterItem_Recipe.cs
+++ Emitters/Items/EmitterItem_Recipe.cs
@@ -7,8 +7,11 @@ namespace Emitters.Items {
 		public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe( this.mod );
 			recipe.AddIngredient( ItemID.FireworkFountain, 1 );
-			recipe.AddIngredient( ItemID.ExplosivePowder, 1 );
-			recipe.AddIngredient( ItemID.Nanites, 1 );
+			recipe.AddIngredient( ItemID.Wire, 1 );
+			recipe.AddIngredient( ItemID.Cloud, 1 );
+			recipe.AddIngredient( ItemID.Flare, 1 );
+			//recipe.AddIngredient( ItemID.ExplosivePowder, 1 );
+			//recipe.AddIngredient( ItemID.Nanites, 1 );
 			recipe.AddTile( TileID.WorkBenches );
 			recipe.SetResult( this );
 			recipe.AddRecipe();
